2cf5feb660046d1ba2e56b0bd4c3b63aafa437be
GermanBluefox <dogafox@gmail.com>
2022-07-22 18:29:17 +0200

    Work in progress

--- src-widgets/modulefederation.config.js
+++ src-widgets/modulefederation.config.js
@@ -1,38 +1,10 @@
-const makeShared = pkgs => {
-    const result = {};
-    pkgs.forEach(
-        packageName => {
-            result[packageName] = {
-                requiredVersion: '*',
-                singleton: true,
-            };
-        },
-    );
-
-    return result;
-};
-
-module.exports = {
-    name: 'vis2materialWidgets',
-    filename: 'customWidgets.js',
-    exposes: {
+const makeFederation = require('@iobroker/vis-widgets-react-dev/modulefederation.config');
+module.exports = makeFederation(
+    'vis2materialWidgets',
+    {
         './Thermostat': './src/Thermostat',
         './Actual': './src/Actual',
         './Static': './src/Static',
         './Switches': './src/Switches',
-    },
-    shared:
-        makeShared([
-            'react',
-            'react-dom',
-            'react-dom/client',
-            'clsx',
-            '@mui/material',
-            '@mui/styles',
-            '@mui/material/styles',
-            '@mui/icons-material',
-            'prop-types',
-            '@iobroker/adapter-react-v5',
-            '@iobroker/vis-widgets-react-dev',
-        ]),
-};
+    }
+);
