4a7ebec51902e6ac79f94ca0b406de97384b1e9c
GermanBluefox <dogafox@gmail.com>
2023-02-22 20:51:12 +0100

    Update packages

--- gulpfile.js
+++ gulpfile.js
@@ -69,6 +69,15 @@ function buildWidgets() {
 
     fs.writeFileSync(`${src}package.json`, JSON.stringify(data, null, 4));
 
+    // we have bug, that federation requires version number in @mui/material/styles, so we have to change it
+    // read version of @mui/material and write it to @mui/material/styles
+    const muiStyleVersion = require(`${src}node_modules/@mui/material/styles/package.json`);
+    if (!muiStyleVersion.version) {
+        const muiVersion = require(`${src}node_modules/@mui/material/package.json`);
+        muiStyleVersion.version = muiVersion.version;
+        fs.writeFileSync(`${src}node_modules/@mui/material/styles/package.json`, JSON.stringify(muiStyleVersion, null, 2));
+    }
+
     return new Promise((resolve, reject) => {
         const options = {
             stdio: 'pipe',
