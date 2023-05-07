b5dec5eb34277d3132d2be02228f2b46a3d87f5e
GermanBluefox <dogafox@gmail.com>
2023-05-07 22:45:03 +0200

    Work on GUI tests

--- test/widgets.test.js
+++ test/widgets.test.js
@@ -12,6 +12,25 @@ let onStateChanged = null;
 
 const VIS_UPLOADED_ID = 'vis-2-beta.0.info.uploaded';
 
+function deleteFoldersRecursive(path) {
+    if (path.endsWith('/')) {
+        path = path.substring(0, path.length - 1);
+    }
+    if (fs.existsSync(path)) {
+        const files = fs.readdirSync(path);
+        for (const file of files) {
+            const curPath = `${path}/${file}`;
+            const stat = fs.statSync(curPath);
+            if (stat.isDirectory()) {
+                deleteFoldersRecursive(curPath);
+                fs.rmdirSync(curPath);
+            } else {
+                fs.unlinkSync(curPath);
+            }
+        }
+    }
+}
+
 function checkValueOfState(id, value, cb, counter) {
     counter = counter === undefined ? 20 : counter;
     if (counter === 0) {
@@ -55,42 +74,39 @@ function checkIsVisUploadedAsync(counter) {
         checkIsVisUploaded(err => {
             if (err) {
                 console.error(err);
-                resolve(err);
+                reject(err);
             } else {
-                reject();
+                resolve();
             }
         }, counter));
 }
 
-function installAdapter(adapterName) {
-    if (!fs.existsSync(`${__dirname}/../tmp/node_modules/iobroker.${adapterName}`)) {
-        console.log(`Install ${adapterName}`);
-        cp.execSync(`npm install iobroker.${adapterName} --production`, {cwd: `${__dirname}/../tmp`});
-    }
-}
-
 function startIoBroker() {
     return new Promise(resolve => {
-        setup.setupController(['web', 'vis-2-beta'], async () => {
-            installAdapter('web');
-            installAdapter('vis-2-beta');
+        // delete the old project
+        deleteFoldersRecursive(`${__dirname}/../tmp/iobroker-data/files/vis-2-beta.0/main`);
+        fs.existsSync(`${__dirname}/../tmp/iobroker-data/files/vis-2-beta.0/main`) && fs.unlinkSync(`${__dirname}/../tmp/iobroker-data/files/vis-2-beta.0/main`);
 
+        setup.setupController(['iobroker.web@5.5.3', 'iobroker.vis-2-beta@2.0.25'], async () => {
             await setup.setOfflineState('vis-2-beta.0.info.uploaded', {val: 0});
             // lets the web adapter start on port 18082
             let config = await setup.getAdapterConfig(0, 'web');
-            config.common.port = 18082;
+            config.native.port = 18082;
             config.common.enabled = true;
             await setup.setAdapterConfig(config.common, config.native, 0, 'web');
 
             config = await setup.getAdapterConfig(0, 'vis-2-beta');
-            config.common.enabled = true;
-            await setup.setAdapterConfig(config.common, config.native, 0, 'vis-2-beta');
+            if (!config.common.enabled) {
+                config.common.enabled = true;
+                await setup.setAdapterConfig(config.common, config.native, 0, 'vis-2-beta');
+            }
 
             setup.startController(
                 false, // do not start widgets
                 (id, obj) => {},
                 (id, state) => onStateChanged && onStateChanged(id, state),
                 (_objects, _states) => {
+                    console.log('STARTED!');
                     objects = _objects;
                     states  = _states;
                     setup.startCustomAdapter('web', 0);
@@ -127,7 +143,7 @@ async function startPuppeteer() {
 
 describe('vis-2-widgets-material', () => {
     before(async function (){
-        this.timeout(120000);
+        this.timeout(180000);
         // install js-controller, web and vis-2-beta
         await startIoBroker();
         await checkIsVisUploadedAsync(100);
@@ -136,21 +152,22 @@ describe('vis-2-widgets-material', () => {
         page = result.page;
 
         await page.goto('http://127.0.0.1:18082/vis-2-beta/edit.html', { waitUntil: 'domcontentloaded' });
-        await page.waitForSelector( '.MuiAvatar-root', { timeout: 30000 } );
+        await page.waitForSelector( '#create-new-project', { timeout: 30000 } );
     });
 
-    it(`Check if vis-2-beta adapter started`, async () => {
+    it(`Check if vis-2-beta adapter started`, async (done) => {
         await checkIsVisUploadedAsync();
-        expect(true).to.be(true);
+        expect(1).to.equal(1);
+        done();
     }).timeout(120000);
 
-    after(async function (done) {
+    after(async function () {
         this.timeout(10000);
         browser.close();
 
         await setup.stopCustomAdapter('vis-2-beta', 0);
         await setup.stopCustomAdapter('web', 0);
 
-        await stopIoBroker();
+        return stopIoBroker();
     });
 });
