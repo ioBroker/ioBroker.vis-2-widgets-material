cd7db3f4e190efaceec7f3639d4a704f41eeeb64
GermanBluefox <dogafox@gmail.com>
2023-10-09 21:26:54 +0200

    Do not import Fan Icon

--- src-widgets/src/Vacuum.jsx
+++ src-widgets/src/Vacuum.jsx
@@ -8,13 +8,23 @@ import {
 import {
     BatteryChargingFull, BatteryFull, Home, PlayArrow,
 } from '@mui/icons-material';
-import { FaFan } from 'react-icons/fa';
 
 import { Icon } from '@iobroker/adapter-react-v5';
 
 import vacuumIcon from './assets/vacuum_icon.svg';
 
 import Generic from './Generic';
+
+const FanIcon = props => <svg
+    viewBox="0 0 512 512"
+    width={props.width || 20}
+    height={props.height || props.width || 20}
+    xmlns="http://www.w3.org/2000/svg"
+    className={props.className}
+    style={props.style}
+>
+    <path fill="currentColor" d="M352.57 128c-28.09 0-54.09 4.52-77.06 12.86l12.41-123.11C289 7.31 279.81-1.18 269.33.13 189.63 10.13 128 77.64 128 159.43c0 28.09 4.52 54.09 12.86 77.06L17.75 224.08C7.31 223-1.18 232.19.13 242.67c10 79.7 77.51 141.33 159.3 141.33 28.09 0 54.09-4.52 77.06-12.86l-12.41 123.11c-1.05 10.43 8.11 18.93 18.59 17.62 79.7-10 141.33-77.51 141.33-159.3 0-28.09-4.52-54.09-12.86-77.06l123.11 12.41c10.44 1.05 18.93-8.11 17.62-18.59-10-79.7-77.51-141.33-159.3-141.33zM256 288a32 32 0 1 1 32-32 32 32 0 0 1-32 32z" />
+</svg>;
 
 const styles = theme => ({
     battery: {
@@ -354,7 +364,7 @@ class Vacuum extends Generic {
             }}
             className={this.props.classes.speed}
         >
-            <FaFan />
+            <FanIcon />
             {this.getValue('fan_speed', true)}
         </Button>;
     }
